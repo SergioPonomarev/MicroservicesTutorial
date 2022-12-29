b32ea540e45b4521eb9343c8095d058b4fba4439
Sergey Ponomarev <sergiopono@gmail.com>
2022-12-29 17:51:59 +0300

    Dtos and Profile are added.

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
