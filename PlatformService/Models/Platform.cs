8779c354481cd146d7ff35c5849858bcef054782
Tahmasib <sergiopono@gmail.com>
2023-01-04 20:04:51 +0400

    Actions added to PlatformController
    Platform type changed form class to record

--- PlatformService/Models/Platform.cs
+++ PlatformService/Models/Platform.cs
@@ -2,19 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PlatformService.Models
 {
-    public class Platform
+    public record Platform
     {
         [Key]
         [Required]
-        public int Id { get; set; }
+        public int Id { get; init; }
 
         [Required]
-        public string Name { get; set; }
+        public string Name { get; init; }
 
         [Required]
-        public string Publisher { get; set; }
+        public string Publisher { get; init; }
 
         [Required]
-        public string Cost { get; set; }
+        public string Cost { get; init; }
     }
 }
