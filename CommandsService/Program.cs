58981e7fb6247abde21a4cc48b4d9c65f8f05b07
Sergey Ponomarev <sergiopono@gmail.com>
2023-03-29 11:10:48 +0300

    CommandsController added.

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,7 +1,12 @@
+using CommandsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMemo"));
+builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
